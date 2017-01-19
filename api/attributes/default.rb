9cd3a99c93787acdc9ef25acaf7423c987c21cd5
phyrokinesis <Pyrokinesis@users.noreply.github.com>
2017-01-19 19:37:43 -0300

    delete

--- api/attributes/default.rb
+++
@@ -1,6 +0,0 @@
-# Deploy
-default['clients-api']['deploy']['env_srv'] = ''
-default['clients-api']['deploy']['env_db'] = ''
-default['clients-api']['deploy']['env_user'] = ''
-default['clients-api']['deploy']['env_pass'] = ''
-default['clients-api']['deploy']['env_file'] = ''
